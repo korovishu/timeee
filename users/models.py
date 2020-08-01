355ce4c6e294f12b8baf7d082f4df35db2c020a1
vishesh <kvishesh89@gmail.com>
2020-08-01 18:21:15 +0530

    anshuman ek no ka chutiya hai

--- users/models.py
+++ users/models.py
@@ -6,8 +6,8 @@ from django.urls import reverse
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-	Roll_No = models.IntegerField()
-	Department = models.CharField(max_length=100)
+	Roll_No = models.IntegerField(default='180101089')
+	Department = models.CharField(default='CSE',max_length=100)
 
 	def __str__(self):
 		return f'{self.user.username} Profile'
